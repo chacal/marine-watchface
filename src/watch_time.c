6c89d4d148abd3e4408170c92213f46c4c7d6eb2
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-05-22 19:35:14 +0300

    Extract method

--- src/watch_time.c
+++ src/watch_time.c
@@ -2,29 +2,21 @@
 #include "fonts.h"
 
 TextLayer* create_time_layer() {
-  // Create the TextLayer with specific bounds
   TextLayer *time_layer = text_layer_create(GRect(0, 20, 144, 60));
-
-  // Improve the layout to be more like a watchface
-  text_layer_set_background_color(time_layer, GColorClear);
-  text_layer_set_text_color(time_layer, GColorBlack);
-  text_layer_set_text(time_layer, "00:00");
-  text_layer_set_font(time_layer, TIME_FONT);
-  text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
-
+  text_layer_set_basic_properties(time_layer, TIME_FONT);
   return time_layer;
 }
 
 TextLayer* create_date_layer() {
-  // Create the TextLayer with specific bounds
   TextLayer *layer = text_layer_create(GRect(0, 3, 144, 23));
+  text_layer_set_basic_properties(layer, DATUM_FONT);
+  return layer;
+}
 
-  // Improve the layout to be more like a watchface
+void text_layer_set_basic_properties(TextLayer* layer, GFont font) {
   text_layer_set_background_color(layer, GColorClear);
   text_layer_set_text_color(layer, GColorBlack);
-  text_layer_set_text(layer, "-");
-  text_layer_set_font(layer, DATUM_FONT);
+  text_layer_set_text(layer, "");
+  text_layer_set_font(layer, font);
   text_layer_set_text_alignment(layer, GTextAlignmentCenter);
-
-  return layer;
 }
--- src/watch_time.h
+++ src/watch_time.h
@@ -3,3 +3,5 @@
 
 TextLayer* create_time_layer();
 TextLayer* create_date_layer();
+
+void text_layer_set_basic_properties(TextLayer* layer, GFont font);
