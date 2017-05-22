1595ae71fdc3d6ffed843262fb1dcb8278972dd0
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-05-22 18:33:54 +0300

    Add more fonts

--- src/fonts.c
+++ src/fonts.c
@@ -1,11 +1,17 @@
 #include "fonts.h"
 
+GFont UNIT_FONT;
+GFont DATUM_FONT;
 GFont TIME_FONT;
 
 void load_fonts() {
+  UNIT_FONT = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_ROBOTO_REGULAR_12));
+  DATUM_FONT = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_ROBOTO_MEDIUM_20));
   TIME_FONT = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_ROBOTO_MEDIUM_56));
 }
 
 void unload_fonts() {
+  fonts_unload_custom_font(UNIT_FONT);
+  fonts_unload_custom_font(DATUM_FONT);
   fonts_unload_custom_font(TIME_FONT);
 }
--- src/fonts.h
+++ src/fonts.h
@@ -2,6 +2,8 @@
 #include <pebble.h>
 
 extern GFont TIME_FONT;
+extern GFont DATUM_FONT;
+extern GFont UNIT_FONT;
 
 void load_fonts();
 void unload_fonts();
